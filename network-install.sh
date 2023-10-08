f0e262d5a1fc5a95aadd4279685c4aaa078c4bc2
omegaui <omegaui22@gmail.com>
2023-10-08 22:19:43 +0530

    🚀 Optimized network install & update.

--- network-install.sh
+++ network-install.sh
@@ -4,13 +4,14 @@ echo "Welcome to the Network Installer"
 echo "This script will download and integrate App Fleet to your system,"
 echo "and will add the workspace launcher to $HOME/.config/autostart directory."
 echo ">> Step 1: Downloading the latest precompiled bundle ..."
+mkdir app-fleet-bundle
+cd app-fleet-bundle
 wget "https://github.com/omegaui/app_fleet/releases/download/v1.0.0/app-fleet-bundle.zip" --output-document='app-fleet-bundle.zip'
 echo ">> Step 2: Extracting the downloaded bundle ..."
-tar -xvf "app-fleet-bundle.zip"
+unzip "app-fleet-bundle.zip"
 echo ">> Step 3: Removing the downloaded bundle to save space ..."
 rm "app-fleet-bundle.zip"
 echo ">> Step 4: Authorize to make the installer executable ..."
-cd app-fleet-bundle
 sudo chmod 0755 install.sh integration/integrate.sh
 echo ">> Step 5: Starting Installer ..."
 ./install.sh
--- network-update.sh
+++ network-update.sh
@@ -2,16 +2,17 @@
 clear
 echo "Welcome to the Network Updater"
 echo "This script will download and reintegrate App Fleet to your system,"
-echo "and will update the workspace launcher located at $HOME/.config/autostart directory."
+echo "and will re-add the workspace launcher to $HOME/.config/autostart directory."
 echo ">> Step 1: Downloading the latest precompiled bundle ..."
-wget "" --output-document='app-fleet-bundle.zip'
-echo ">> Step 2: Extracting the downloaded bundle ..."
+mkdir app-fleet-bundle
+cd app-fleet-bundle
 wget "https://github.com/omegaui/app_fleet/releases/download/v1.0.0/app-fleet-bundle.zip" --output-document='app-fleet-bundle.zip'
 echo ">> Step 2: Extracting the downloaded bundle ..."
-tar -xvf "app-fleet-bundle.zip"
+unzip "app-fleet-bundle.zip"
+echo ">> Step 3: Removing the downloaded bundle to save space ..."
+rm "app-fleet-bundle.zip"
 echo ">> Step 4: Authorize to make the updater executable ..."
-cd app-fleet-bundle
 sudo chmod 0755 update.sh integration/integrate.sh
-echo ">> Step 5: Starting Updater ..."
+echo ">> Step 5: Starting Installer ..."
 ./update.sh
 echo ">> Thank you for updating App Fleet!"
