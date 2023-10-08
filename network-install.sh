a7dbcdb27474c7188ea6e6ca8971690447a36259
omegaui <omegaui22@gmail.com>
2023-10-08 22:11:36 +0530

    🚀 v1.0.0 published.

--- network-install.sh
+++ network-install.sh
@@ -4,9 +4,9 @@ echo "Welcome to the Network Installer"
 echo "This script will download and integrate App Fleet to your system,"
 echo "and will add the workspace launcher to $HOME/.config/autostart directory."
 echo ">> Step 1: Downloading the latest precompiled bundle ..."
-wget "" --output-document='app-fleet-bundle.zip'
+wget "https://github.com/omegaui/app_fleet/releases/download/v1.0.0/app-fleet-bundle.zip" --output-document='app-fleet-bundle.zip'
 echo ">> Step 2: Extracting the downloaded bundle ..."
-tar xvcf "app-fleet-bundle.zip"
+tar -xvf "app-fleet-bundle.zip"
 echo ">> Step 3: Removing the downloaded bundle to save space ..."
 rm "app-fleet-bundle.zip"
 echo ">> Step 4: Authorize to make the installer executable ..."
--- network-update.sh
+++ network-update.sh
@@ -6,9 +6,9 @@ echo "and will update the workspace launcher located at $HOME/.config/autostart 
 echo ">> Step 1: Downloading the latest precompiled bundle ..."
 wget "" --output-document='app-fleet-bundle.zip'
 echo ">> Step 2: Extracting the downloaded bundle ..."
-tar xvcf "app-fleet-bundle.zip"
-echo ">> Step 3: Removing the downloaded bundle to save space ..."
-rm "app-fleet-bundle.zip"
+wget "https://github.com/omegaui/app_fleet/releases/download/v1.0.0/app-fleet-bundle.zip" --output-document='app-fleet-bundle.zip'
+echo ">> Step 2: Extracting the downloaded bundle ..."
+tar -xvf "app-fleet-bundle.zip"
 echo ">> Step 4: Authorize to make the updater executable ..."
 cd app-fleet-bundle
 sudo chmod 0755 update.sh integration/integrate.sh
