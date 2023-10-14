0bfc28050f6f46309a83816a941f8b49f464387c
omegaui <omegaui22@gmail.com>
2023-10-14 23:59:50 +0530

    🎗 Meta info update (build 5)

--- lib/constants/app_meta_info.dart
+++ lib/constants/app_meta_info.dart
@@ -1,7 +1,7 @@
 class AppMetaInfo {
   AppMetaInfo._();
 
-  static const version = "v1.0.0+4";
+  static const version = "v1.0.0+5";
   static const releasePageUrl =
       "https://github.com/omegaui/app-fleet/releases/latest";
 }
